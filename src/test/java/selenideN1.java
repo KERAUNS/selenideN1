e855563f23c27fd353baa809816c59a0cadc0f2d
kerauns <vesstar.dj@gmail.com>
2023-03-28 01:03:28 +0300

    check code realization finished

--- src/test/java/selenideN1.java
+++ src/test/java/selenideN1.java
@@ -1,7 +1,7 @@
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.conditions.Text.text;
-
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.selector.ByText;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,14 +9,26 @@ import org.junit.jupiter.api.Test;
 public class selenideN1 {
     @BeforeAll
     static void beforeAll() {
-       Configuration.baseUrl = "https://github.com";
-       Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://github.com";
+        Configuration.holdBrowserOpen = true;
     }
     @Test
-    void selenideWikiGitCheckSoftAssertions () {
+    void selenideWikiGitCheckSoftAssertions() {
+        String code = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
         open("/selenide/selenide");
         $("#wiki-tab").click();
-
-        //open("/selenide/selenide/wiki");
+        $(new ByText("Soft assertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").shouldHave(Condition.href("#3-using-junit5-extend-test-class"));
+       $("#repo-content-turbo-frame").shouldHave(text(code));
     }
 };
