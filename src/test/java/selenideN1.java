b7e4da05621372d7b9b83e9876307e06d189d613
KERAUNS <vesstar.dj@gmail.com>
2023-04-24 01:09:29 +0300

    checking page and choosing maked correctly

--- src/test/java/selenideN1.java
+++ src/test/java/selenideN1.java
@@ -13,21 +13,10 @@ public class selenideN1 {
     }
     @Test
     void selenideWikiGitCheckSoftAssertions() {
-        String code = "@ExtendWith({SoftAssertsExtension.class})\n" +
-                "class Tests {\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }\n" +
-                "}";
-        open("/selenide/selenide");
+       open("/selenide/selenide");
         $("#wiki-tab").click();
         $("input#wiki-pages-filter").setValue("SoftAssertions");
         $$("ul.list-style-none.filterable-active li").get(19).$("a").shouldHave(matchText("SoftAssertions")).click();
         $("h1").shouldHave(text("SoftAssertions"));
     }
-};
+}
