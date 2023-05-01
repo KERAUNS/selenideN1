44665d19353d6e71b70cbb2ec07fb2fb2f247e2b
KERAUNS <vesstar.dj@gmail.com>
2023-05-01 16:08:21 +0300

    fix test

--- src/test/java/selenideN1.java
+++ src/test/java/selenideN1.java
@@ -1,5 +1,5 @@
-import static com.codeborne.selenide.Condition.matchText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,13 +10,28 @@ public class selenideN1 {
     static void beforeAll() {
         Configuration.baseUrl = "https://github.com";
         Configuration.holdBrowserOpen = true;
+
     }
     @Test
     void selenideWikiGitCheckSoftAssertions() {
+        String code = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
        open("/selenide/selenide");
         $("#wiki-tab").click();
         $("input#wiki-pages-filter").setValue("SoftAssertions");
-        $$("ul.list-style-none.filterable-active li").get(19).$("a").shouldHave(matchText("SoftAssertions")).click();
-        $("h1").shouldHave(text("SoftAssertions"));
+        $(".filterable-active").shouldHave(text("SoftAssertions"))
+                .$(byText("SoftAssertions")).click();
+        $("#repo-content-turbo-frame").shouldHave(text(code));
     }
 }
